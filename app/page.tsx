53e37d02a33bf49e01b8aea750242c20b157347d
mohd-hassan17 <ansarimohdhassan3@gmail.com>
2025-11-19 01:46:39 +0530

    new files

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="text-center mt-20 flex flex-col items-center">
-      <h1>AskCortex</h1>
+      <h1>AskCortexx</h1>
     </div>
   );
 }
